6b276be96a25aa772dd482be3bea695d4c6016e5
icsalgado <icsalgado@icloud.com>
2021-02-02 16:34:29 -0300

    criando movimento

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8* box,
     y: 8* box
 }
+let direction = "right";
 
 function criarBG() {
     //pinta o funco do canvas
@@ -21,5 +22,31 @@ function criarSnake(){
     }
 }
 
-criarBG();
-criarSnake();
+function iniciarGame(){
+    criarBG();
+    criarSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+
+    if(direction == "left") snakeX -= box;
+
+    if(direction == "up") snakeY += box;
+
+    if(direction == "down") snakeY -= box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+
+
+let jogo = setInterval(iniciarGame, 100);
