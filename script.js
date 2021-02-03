a4bc41c67913477e12c5664f2a5a2f17b7f8af42
icsalgado <icsalgado@icloud.com>
2021-02-03 15:59:54 -0300

    Finalizando o jogo e incluindo limites para game over

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box =32;
+let box = 32;
 let snake = [];
 snake[0] = {
-    x: 8* box,
-    y: 8* box
+    x: 8 * box,
+    y: 8 * box
 }
 let direction = "right";
 
@@ -39,34 +39,53 @@ function update (event){
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
+function gameOver(){
+    clearInterval(jogo);
+    alert('GAMEOVER');
+    window.location.reload();
+}
+
+
 function iniciarGame(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    //definindo os limites do jogo    
+    if(snake[0].x > 15 * box && direction == "right") gameOver();
+    if(snake[0].x < 0 && direction == "left") gameOver();
+    if(snake[0].y > 15 * box && direction == "down") gameOver();
+    if(snake[0].y < 0 && direction == "up") gameOver();
+    //jogo sem limites
+    // if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    // if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    // if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    // if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    //se a snake se bater, morre
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) gameOver();
+    }
 
     criarBG();
     criarSnake();
     criaFood();
 
+    //faz a snake andar
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
-
+    
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
-
+  
+    //troca a posição da food se a snake comer    
     if(snakeX != food.x || snakeY != food.y){
         snake.pop();
     }
     else{
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
-    }
+    }    
 
     
-
     let newHead = {
         x: snakeX,
         y: snakeY
